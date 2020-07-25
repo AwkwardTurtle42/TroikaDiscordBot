01b498c7f099f711bc84e1197c84a73b2a119bb5
Jacob Harris <harrisj.home@gmail.com>
2020-07-25 18:16:51 -0400

    Weapon lookup and standard weapons

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,4 @@
-from troika_discord.utils import *
+from troika_discord.util import *
 import mock
 
 def test_roll_d6():
@@ -22,7 +22,7 @@ def test_oops_hit():
     for r1 in range(1, 6):
         for r2 in range(1, 6):
             total = r1*10 + r2
-            with mock.patch('troika_discord.utils.roll_d66', return_value=total):
+            with mock.patch('troika_discord.util.roll_d66', return_value=total):
                 roll, msg = roll_oops()
                 assert roll == total
                 assert msg == OOPS_TABLE[total]
